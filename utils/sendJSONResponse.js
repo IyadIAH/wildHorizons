6339edc55359d4833ff92697b0c03d6e3f4524bb
Iyad Hmidat <iyad@Iyadhmidat.>
2025-04-19 16:17:58 +0300

    Adding CORS policy to only allow GET method

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,7 @@
 export const sendJSONResponse = (res, statusCode, payload) => {
     res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-origin', '*')
+    res.setHeader('Access-Control-Allow-Method', 'GET')
     res.statusCode = statusCode
     res.end(JSON.stringify(payload))
 }
